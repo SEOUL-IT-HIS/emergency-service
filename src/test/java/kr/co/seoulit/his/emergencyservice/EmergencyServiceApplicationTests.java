bd2ac6ad8dc56904736dc3cfd7ddae1b2f014054
sangmin <tldmsdl1341@gmail.com>
2026-07-07 11:16:33 +0900

    커밋

---
+++ src/test/java/kr/co/seoulit/his/emergencyservice/EmergencyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.seoulit.his.emergencyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
